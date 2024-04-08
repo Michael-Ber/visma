12852ca7f487dc164d103d091d2e535d1e99b53b
unknown <mikeber000@gmail.com>
2024-04-08 19:56:33 +0800

    on new gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,12 @@ import webpack from 'webpack-stream';
 import browsersync from 'browser-sync';
 import gulpSass from 'gulp-sass';
 import nodeSass from 'node-sass';
-import autoprefixer from 'autoprefixer';
+import autoprefixer from 'gulp-autoprefixer';
 import cleanCSS from 'gulp-clean-css';
-import postcss from 'gulp-postcss';
-import imagemin, { svgo } from 'gulp-imagemin';
+import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
 import htmlmin from 'gulp-htmlmin';
 import plumber from 'gulp-plumber';
-import webpConv from 'gulp-webp';
+import webp from 'gulp-webp';
 import multiDest from 'gulp-multi-dest';
 import changed from 'gulp-changed';
 
@@ -20,15 +19,24 @@ import notify from 'gulp-notify';
 import sourcemaps from 'gulp-sourcemaps';
 import fileinclude from 'gulp-file-include';
 import svgSprite from 'gulp-svg-sprite';
-
-
+import svgmin from 'gulp-svgmin';
+import cheerio from 'gulp-cheerio';
+import replace from 'gulp-replace';
+import uglify from 'gulp-uglify-es';
+import { deleteAsync } from 'del';
+import gulpif from 'gulp-if';
 
 const sass = gulpSass(nodeSass);
 
 // const dist = "../../../js/OpenServer/domains/portfolio";
 const dist = "./dist";
+let isProd = false;
 
-gulp.task("copy-html", () => {
+gulp.task("clean", () => {
+  return deleteAsync('./dist')
+})
+
+gulp.task("html-minify", () => {
   return gulp.src("./dist/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest(dist))
@@ -45,96 +53,179 @@ gulp.task("html-include", () => {
     .pipe(browsersync.stream())
 })
 
-gulp.task("build-sass", () => {
-  return gulp.src("./src/assets/sass/style.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'expanded'}).on('error', notify.onError()))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dist))
+gulp.task("styles", () => {
+  return gulp.src("./src/assets/sass/style.scss", { sourcemaps: !isProd })
+    .pipe(plumber(
+      notify.onError({
+        title: "SCSS",
+        message: "Error: <%= error.message %>"
+      })
+    ))
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false,
+      grid: true
+    }))
+    .pipe(gulpif(isProd, cleanCSS({
+      level: 2
+    })))
+    .pipe(gulp.dest(dist, { sourcemaps: '.' }))
+    .pipe(browsersync.stream());
+});
+gulp.task("stylesBackend", () => {
+  return gulp.src("./src/assets/sass/style.scss", { sourcemaps: !isProd })
+    .pipe(plumber(
+      notify.onError({
+        title: "SCSS",
+        message: "Error: <%= error.message %>"
+      })
+    ))
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false,
+      grid: true
+    }))
+    .pipe(gulp.dest(dist, { sourcemaps: '.' }))
     .pipe(browsersync.stream());
 });
 
-gulp.task("build-js", () => {
+
+gulp.task("scripts", () => {
   return gulp.src("./src/assets/js/main.js")
+    .pipe(plumber(
+      notify.onError({
+        title: "JS",
+        message: "Error: <%= error.message %>"
+      })
+    ))
+    .pipe(webpack({
+      mode: isProd ? 'production' : 'development',
+      output: {
+        filename: 'script.js',
+      },
+      module: {
+        rules: [{
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  targets: "defaults"
+                }]
+              ]
+            }
+          }
+        }]
+      },
+      devtool: !isProd ? 'source-map' : false
+    }))
+    .on('error', function (err) {
+      console.error('WEBPACK ERROR', err);
+      this.emit('end');
+    })
+    .pipe(gulp.dest(dist))
+    .pipe(browsersync.stream());
+})
+
+gulp.task("scriptsBackend", () => {
+  return gulp.src("./src/assets/js/main.js")
+    .pipe(plumber(
+      notify.onError({
+        title: "JS",
+        message: "Error: <%= error.message %>"
+      })
+    ))
     .pipe(webpack({
       mode: 'development',
       output: {
-        filename: 'script.js'
+        filename: 'script.js',
       },
-      watch: false,
-      devtool: "source-map",
       module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [['@babel/preset-env', {
-                  debug: true,
-                  corejs: 3,
-                  useBuiltIns: "usage"
-                }]]
-              }
+        rules: [{
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  targets: "defaults"
+                }]
+              ]
             }
-          },
-          {
-            test: /\.(sass|less|css)$/,
-            use: ["style-loader", "css-loader", 'sass-loader'],
           }
-        ]
-      }
+        }]
+      },
+      devtool: false
     }))
+    .on('error', function (err) {
+      console.error('WEBPACK ERROR', err);
+      this.emit('end');
+    })
     .pipe(gulp.dest(dist))
-    .on("end", browsersync.reload);
-});
+    .pipe(browsersync.stream());
+})
 
-gulp.task('imagemin', function () {
-  return gulp.src('./src/assets/img/**/*.*')
-    .pipe(imagemin())
+
+gulp.task('images', function () {
+  return gulp.src(['./src/assets/img/**/**.{jpg,jpeg,png,svg}'])
+    .pipe(gulpif(isProd, imagemin([
+      mozjpeg({
+        quality: 80,
+        progressive: true
+      }),
+      optipng({
+        optimizationLevel: 2
+      }),
+    ])))
     // .pipe(gulp.dest('../../../js/OpenServer/domains/portfolio/assets/img'));
     .pipe(gulp.dest('./dist/assets/img'));
 });
 
-gulp.task('iconsmin', function () {
-  return gulp.src('./src/assets/icons/**/*.*')
-    .pipe(imagemin())
-    // .pipe(gulp.dest('../../../js/OpenServer/domains/portfolio/assets/icons'));
-    .pipe(gulp.dest('./dist/assets/icons'));
-});
-
 gulp.task('webp', function () {
-  return gulp.src('./src/assets/img/**/*.*')
-    .pipe(plumber())
-    .pipe(changed('dist/assets/img/webp', {
-      extension: '.webp'
-    }))
-    .pipe(webpConv())
+  return gulp.src(['./src/assets/img/**/**.{jpg,jpeg,png}'])
+    .pipe(webp())
     .pipe(multiDest(['src/assets/img/webp', 'dist/assets/img/webp']))
 })
 
 gulp.task('svgSprites', function () {
-  return gulp.src('./src/assets/icons/*.svg')
-    .pipe(svgSprite({
-      
-      mode: {
-        stack: {
-          sprite: "../sprite.svg"
-        }
-      },
-      svg: {
-        transform: [
-          function(svg) {
-            return svg.replace(/fill=".*?"/ig, '');
+  return gulp.src('./src/assets/svg/*.svg')
+      .pipe(
+        svgmin({
+          js2svg: {
+            pretty: true,
+          },
+        })
+      )
+      .pipe(
+        cheerio({
+          run: function ($) {
+            $('[fill]').removeAttr('fill');
+            $('[stroke]').removeAttr('stroke');
+            $('[style]').removeAttr('style');
+          },
+          parserOptions: {
+            xmlMode: true
+          },
+        })
+      )
+      .pipe(replace('&gt;', '>'))
+      .pipe(svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg"
           }
-        ]
-      }
-    }))
-
-    .pipe(gulp.dest('./dist/assets/icons'))
+        },
+      }))
+    .pipe(gulp.dest('./dist/assets/svg'))
 })
+
+const toProd = (done) => {
+  isProd = true;
+  done();
+};
 
 gulp.task("watch", () => {
   browsersync.init({
@@ -143,59 +234,18 @@ gulp.task("watch", () => {
     notify: true
   });
 
+  gulp.watch("./src/assets/sass/**/*.scss", gulp.parallel("styles"));
+  gulp.watch("./src/assets/js/**/*.js", gulp.parallel("scripts"));
   gulp.watch("./src/partials/**/*.html", gulp.parallel("html-include"));
   gulp.watch("./src/*.html", gulp.parallel("html-include"));
-  gulp.watch("./src/assets/js/**/*.js", gulp.parallel("build-js"));
-  gulp.watch("./src/assets/sass/**/*.scss", gulp.parallel("build-sass"));
-  gulp.watch("./src/assets/img/**/*.*", gulp.parallel("imagemin"));
-  gulp.watch("./src/assets/icons/**/*.*", gulp.parallel("iconsmin"));
-  gulp.watch("./src/assets/icons/**/*.*", gulp.parallel("svgSprites"));
+  gulp.watch("./src/assets/svg/**/*.*", gulp.parallel("svgSprites"));
+  gulp.watch("./src/assets/img/**/**.{jpg,jpeg,png,svg}", gulp.parallel("images"));
+  gulp.watch("./src/assets/img/**/**.{jpg,jpeg,png}", gulp.parallel("webp"));
 });
 
-gulp.task("build", gulp.series("copy-html", "build-js", "build-sass", "svgSprites"));
 
-gulp.task("prod", () => {
-  gulp.src("./src/assets/sass/style.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'expanded'}).on('error', notify.onError()))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(cleanCSS({level: 2}))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dist))
+gulp.task("default", gulp.series("clean", "html-include", "scripts", "styles", "images", "webp", "svgSprites", "watch"));
 
-  return gulp.src("./src/assets/js/main.js")
-    .pipe(webpack({
-      mode: 'production',
-      output: {
-        filename: 'script.js'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: [
-              {
-                loader: 'babel-loader',
-                options: {
-                  presets: [['@babel/preset-env', {
-                    corejs: 3,
-                    useBuiltIns: "usage"
-                  }]]
-                }
-              }
-            ]
-          },
-          {
-            test: /\.(sass|less|css)$/,
-            use: ['style-loader', 'css-loader', 'less-loader']
-          }
-        ],
+gulp.task("build", gulp.series(toProd, "clean", "html-include", "scripts", "styles", "images", "webp", "svgSprites", "html-minify"));
 
-      }
-    }))
-    .pipe(gulp.dest(dist));
-});
-
-gulp.task("default", gulp.parallel("watch", "build"));
+gulp.task("backend", gulp.series("clean", "html-include", "scriptsBackend", "stylesBackend", "images", "webp", "svgSprites"));
